51b6839c8693949819635844f0e9ccdce6c33d63
Iain Simmons <emptiful@gmail.com>
2024-03-04 22:47:31 +1000

    Fix bufferline

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -145,16 +145,49 @@ return {
   },
   {
     "akinsho/bufferline.nvim",
-    { "BufReadPost", "BufNewFile", "BufWritePre" },
+    event = "VeryLazy",
     opts = {
       options = {
         buffer_close_icon = "",
         separator_style = "thin",
+        close_command = function(n)
+          require("mini.bufremove").delete(n, false)
+        end,
+      -- stylua: ignore
+      right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
+        diagnostics = "nvim_lsp",
+        always_show_bufferline = false,
+        diagnostics_indicator = function(_, _, diag)
+          local icons = {
+            Error = " ",
+            Warn = " ",
+            Hint = " ",
+            Info = " ",
+          }
+          local ret = (diag.error and icons.Error .. diag.error .. " " or "") .. (diag.warning and icons.Warn .. diag.warning or "")
+          return vim.trim(ret)
+        end,
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "Neo-tree",
+            highlight = "Directory",
+            text_align = "left",
+          },
+        },
       },
     },
-  },
-  {
-    "folke/which-key.nvim",
+    config = function(_, opts)
+      require("bufferline").setup(opts)
+      -- Fix bufferline when restoring a session
+      vim.api.nvim_create_autocmd("BufAdd", {
+        callback = function()
+          vim.schedule(function()
+            pcall(nvim_bufferline)
+          end)
+        end,
+      })
+    end,
   },
   {
     "rasulomaroff/reactive.nvim",
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -133,6 +133,10 @@ return {
               },
               -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
               -- diagnostics = { disable = { 'missing-fields' } },
+              diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { "vim", "nvim_bufferline" },
+              },
             },
           },
         },
