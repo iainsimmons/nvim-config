b18394887c52d6e1af7dda0ffb7663b411fe8f0a
Iain Simmons <emptiful@gmail.com>
2024-03-04 22:16:09 +1000

    Moved conform to formatting.lua plugin file

--- init.lua
+++ init.lua
@@ -168,27 +168,6 @@ require("lazy").setup({
 
     -- NOTE: Moved LSP to `lua/plugins/lsp.lua`
 
-    { -- Autoformat
-      "stevearc/conform.nvim",
-      event = { "BufNewFile", "BufReadPost" },
-      opts = {
-        notify_on_error = false,
-        format_on_save = {
-          timeout_ms = 500,
-          lsp_fallback = true,
-        },
-        formatters_by_ft = {
-          lua = { "stylua" },
-          -- Conform can also run multiple formatters sequentially
-          -- python = { "isort", "black" },
-          --
-          -- You can use a sub-list to tell conform to run *until* a formatter
-          -- is found.
-          -- javascript = { { "prettierd", "prettier" } },
-        },
-      },
-    },
-
     { -- Autocompletion
       "hrsh7th/nvim-cmp",
       event = "InsertEnter",
@@ -283,6 +262,9 @@ require("lazy").setup({
       end,
     },
 
+    --
+    -- NOTE: Moved conform to `lua/plugins/formatting.lua`
+    --
     -- NOTE: Moved todo-comments to `/lua/plugins/comment.lua`
 
     { -- Collection of various small independent plugins/modules
