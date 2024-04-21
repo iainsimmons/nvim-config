925b59961150f35c4eaaa86a39944b3154e8be50
Iain Simmons <emptiful@gmail.com>
2024-04-21 22:51:14 +1000

    Fix copy current file keymap

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,7 @@ vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir-kickstart"
 vim.opt.wrap = true
 vim.opt.conceallevel = 0
 vim.opt.diffopt = "internal,filler,closeoff,vertical"
+vim.opt.termguicolors = true
 
 -- Add .psv extension for Pipe Separated Values format
 vim.filetype.add({
