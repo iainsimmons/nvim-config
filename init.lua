b41c414357b68ad480ed1ec713f96b3b91b08cca
Iain Simmons <emptiful@gmail.com>
2024-04-28 23:44:43 +1000

    Switch lazy to true by default

--- init.lua
+++ init.lua
@@ -114,9 +114,8 @@ vim.opt.rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require("lazy").setup({
   defaults = {
-    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
-    lazy = false,
+    -- set to `true` to have all custom plugins lazy-loaded by default
+    lazy = true,
     -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
     -- have outdated releases, which may break your Neovim install.
     version = false, -- always use the latest git commit
