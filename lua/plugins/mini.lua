f0258f11d8b08c6e5593e3f13c3ec1c61af7d16f
Iain Simmons <emptiful@gmail.com>
2024-04-28 16:56:30 +1000

    Add mini.operators and sort mini plugins setup calls

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -12,23 +12,6 @@ return { -- Collection of various small independent plugins/modules
       n_lines = 500,
     })
 
-    require("mini.surround").setup({
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - gsaiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - gsd'   - [S]urround [D]elete [']quotes
-      -- - gsr)'  - [S]urround [R]eplace [)] [']
-      mappings = {
-        add = "gsa", -- Add surrounding in Normal and Visual modes
-        delete = "gsd", -- Delete surrounding
-        find = "gsf", -- Find surrounding (to the right)
-        find_left = "gsF", -- Find surrounding (to the left)
-        highlight = "gsh", -- Highlight surrounding
-        replace = "gsr", -- Replace surrounding
-        update_n_lines = "gsn", -- Update `n_lines`
-      },
-    })
-
     require("mini.bracketed").setup()
 
     require("mini.comment").setup({
@@ -108,6 +91,69 @@ return { -- Collection of various small independent plugins/modules
         -- Tweak left-hand side of mapping to your liking
         vim.keymap.set("n", "g.", toggle_dotfiles, { buffer = buf_id, desc = "Toggle Hidden Files" })
       end,
+    })
+
+    require("mini.operators").setup({
+      -- Each entry configures one operator.
+      -- `prefix` defines keys mapped during `setup()`: in Normal mode
+      -- to operate on textobject and line, in Visual - on selection.
+
+      -- Evaluate text and replace with output
+      evaluate = {
+        prefix = "g=",
+
+        -- Function which does the evaluation
+        func = nil,
+      },
+
+      -- Exchange text regions
+      exchange = {
+        prefix = "gx",
+
+        -- Whether to reindent new text to match previous indent
+        reindent_linewise = true,
+      },
+
+      -- Multiply (duplicate) text
+      multiply = {
+        prefix = "gm",
+
+        -- Function which can modify text before multiplying
+        func = nil,
+      },
+
+      -- Replace text with register
+      replace = {
+        prefix = "gp",
+
+        -- Whether to reindent new text to match previous indent
+        reindent_linewise = true,
+      },
+
+      -- Sort text
+      sort = {
+        prefix = "gs",
+
+        -- Function which does the sort
+        func = nil,
+      },
+    })
+
+    require("mini.surround").setup({
+      -- Add/delete/replace surroundings (brackets, quotes, etc.)
+      --
+      -- - gsaiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+      -- - gsd'   - [S]urround [D]elete [']quotes
+      -- - gsr)'  - [S]urround [R]eplace [)] [']
+      mappings = {
+        add = "gsa", -- Add surrounding in Normal and Visual modes
+        delete = "gsd", -- Delete surrounding
+        find = "gsf", -- Find surrounding (to the right)
+        find_left = "gsF", -- Find surrounding (to the left)
+        highlight = "gsh", -- Highlight surrounding
+        replace = "gsr", -- Replace surrounding
+        update_n_lines = "gsn", -- Update `n_lines`
+      },
     })
   end,
   keys = {
