334119bd75cbf59db917e27dd27ee384fbe5b769
Iain Simmons <emptiful@gmail.com>
2024-04-28 23:18:40 +1000

    Fix deprecation notice of inlay hints (#873)

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -75,7 +75,7 @@ return {
           -- This may be unwanted, since they displace some of your code
           if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
             map("<leader>uh", function()
-              vim.lsp.inlay_hint.enable(0, not vim.lsp.inlay_hint.is_enabled())
+              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
             end, "Toggle Inlay [H]ints")
           end
         end,
