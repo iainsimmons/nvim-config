707ea1e5b94ee94fbcfd610cb04783db0e4e4430
Iain Simmons <emptiful@gmail.com>
2024-04-28 21:20:40 +1000

    remove lazy.nvim configuration (empty) table (#652)

--- init.lua
+++ init.lua
@@ -201,7 +201,7 @@ require("lazy").setup({
     -- import plugins
     { import = "plugins" },
   },
-}, {})
+})
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
