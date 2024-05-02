5a96b1d67a508536fbce2676d6296d57b93caf3d
Iain Simmons <emptiful@gmail.com>
2024-05-02 15:36:15 +1000

    Change tab spacing options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,6 +3,9 @@ vim.opt.wrap = true
 vim.opt.conceallevel = 0
 vim.opt.diffopt = "internal,filler,closeoff,vertical"
 vim.opt.termguicolors = true
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
 
 -- Add .psv extension for Pipe Separated Values format
 vim.filetype.add({
