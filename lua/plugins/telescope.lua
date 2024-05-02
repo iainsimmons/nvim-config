6e18570d3dc5af10a8888ed624121617712f06a6
Iain Simmons <emptiful@gmail.com>
2024-05-02 15:36:52 +1000

    Add in colorscheme/theme search keymap for Telescope
    `<leader>st` for [S]earch [T]hemes

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -94,6 +94,13 @@ return {
       vim.keymap.set("n", "<leader>sn", function()
         builtin.find_files({ cwd = vim.fn.stdpath("config") })
       end, { desc = "[S]earch [N]eovim files" })
+
+      vim.keymap.set("n", "<leader>st", ":Telescope colorscheme enable_preview=true<CR>", {
+        silent = true,
+        noremap = true,
+        desc = "[S]earch [T]hemes",
+      })
+
       telescope.setup(opts)
     end,
     opts = {
