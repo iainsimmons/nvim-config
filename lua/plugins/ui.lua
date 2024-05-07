b79ee856f9d621000a1b9605aaea9b66a4dc191e
Iain Simmons <emptiful@gmail.com>
2024-05-07 21:37:06 +1000

    Remove autocommand for nvim theme changing on focus
    Too buggy, statusline, highlights, etc was messed up

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -16,35 +16,15 @@ return {
         end
       end
 
-      local function handle_focus_gained()
-        local new_theme_name = get_tinty_theme()
-        local current_theme_name = vim.g.colors_name
-
-        if current_theme_name ~= new_theme_name then
-          vim.cmd("colorscheme " .. new_theme_name)
-        end
-      end
-
       local function main()
         vim.o.termguicolors = true
         vim.g.tinted_colorspace = 256
         local current_theme_name = get_tinty_theme()
 
         vim.cmd("colorscheme " .. current_theme_name)
-
-        vim.api.nvim_create_autocmd("FocusGained", {
-          callback = handle_focus_gained,
-        })
       end
 
       main()
-      -- local cmd = vim.cmd
-      -- local g = vim.g
-      -- local current_theme_name = os.getenv("BASE16_THEME") or "default-dark"
-      -- if current_theme_name and g.colors_name ~= "base16-" .. current_theme_name then
-      --   g.base16colorspace = 256
-      --   cmd("colorscheme base16-" .. current_theme_name)
-      -- end
     end,
   },
   {
