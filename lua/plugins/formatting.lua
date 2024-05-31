fcab3a6b6497d81780cbc25a7acea1a20f5ba68f
Iain Simmons <emptiful@gmail.com>
2024-05-31 16:46:35 +1000

    Add keymap to toggle autoformat on save
    `\\f` (Backslash f)
    
    Also changed conform.nvim event to BufWritePre

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,12 +1,15 @@
 return {
   "stevearc/conform.nvim",
-  event = { "BufNewFile", "BufReadPost" },
+  event = { "BufWritePre" },
   opts = {
     notify_on_error = false,
-    format_on_save = {
-      timeout_ms = 1000,
-      lsp_fallback = true,
-    },
+    format_on_save = function(bufnr)
+      -- Disable with a global or buffer-local variable
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+      end
+      return { timeout_ms = 1000, lsp_fallback = true, quiet = true }
+    end,
     formatters_by_ft = {
       lua = { "stylua" },
       fish = { "fish_indent" },
@@ -32,13 +35,44 @@ return {
       rss = { "xmlformatter" },
     },
   },
+  config = function(_, opts)
+    vim.api.nvim_create_user_command("FormatDisable", function(args)
+      if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+    end, {
+      desc = "Disable autoformat-on-save",
+      bang = true,
+    })
+    vim.api.nvim_create_user_command("FormatEnable", function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, {
+      desc = "Re-enable autoformat-on-save",
+    })
+    vim.keymap.set("n", [[\f]], function()
+      if vim.g.disable_autoformat then
+        vim.b.disable_autoformat = false
+        vim.g.disable_autoformat = false
+        vim.notify("Autoformat enabled")
+      else
+        vim.b.disable_autoformat = true
+        vim.g.disable_autoformat = true
+        vim.notify("Autoformat disabled")
+      end
+    end, { desc = "Toggle autoformatting" })
+    require("conform").setup(opts)
+  end,
   keys = {
     {
       "<leader>cf",
       function()
         require("conform").format({ async = true, timeout_ms = 1000, lsp_fallback = true })
       end,
-      mode = "",
+      mode = "n",
       desc = "[F]ormat buffer",
     },
   },
