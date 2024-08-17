99472a0f13b78b61b58541f6c5a0b1111e075241
Iain Simmons <emptiful@gmail.com>
2024-08-17 22:54:44 +1000

    Add explicit dependency of nvim-lspconfig on cmp-nvim-lsp (#1042)

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,6 +11,9 @@ return {
       -- Useful status updates for LSP.
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
       { "j-hui/fidget.nvim", opts = {} },
+
+      -- Allows extra capabilities provided by nvim-cmp
+      "hrsh7th/cmp-nvim-lsp",
     },
     config = function()
       --  This function gets run when an LSP attaches to a particular buffer.
