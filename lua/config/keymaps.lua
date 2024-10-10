a37cb53cf75965eb65a5054526402afeb42da37c
Iain Simmons <emptiful@gmail.com>
2024-10-10 16:39:05 +1000

    Update keymap for viewing diagnostics in quickfix list
    `<leader>xd`

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,7 +13,7 @@ end
 map("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Diagnostic keymaps
-map("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+map("n", "<leader>xd", vim.diagnostic.setloclist, { desc = "Diagnostics (Quickfix)" })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
