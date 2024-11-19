dd49910b1aa0895edae4b31f9cd0d86f7f355401
Iain Simmons <emptiful@gmail.com>
2024-11-19 22:28:47 +1000

    Revert "Switch to blink.cmp for completions"
    This reverts commit 42968cea97b02d476a626af91914e0d8649eeee7.

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,68 +1,133 @@
-return {
-  {
-    "saghen/blink.cmp",
-    lazy = false, -- lazy loading handled internally
-    -- optional: provides snippets for the snippet source
-    dependencies = "rafamadriz/friendly-snippets",
-
-    -- use a release tag to download pre-built binaries
-    version = "v0.*",
-    -- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-    -- build = 'cargo build --release',
-    -- If you use nix, you can build from source using latest nightly rust with:
-    -- build = 'nix run .#build-plugin',
-
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
-    opts = {
-      -- 'default' for mappings similar to built-in completion
-      -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-      -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-      -- see the "default configuration" section below for full documentation on how to define
-      -- your own keymap.
-      keymap = {
-        ---@diagnostic disable-next-line: assign-type-mismatch
-        preset = "default",
-        ["<C-space>"] = { "accept", "show", "show_documentation", "hide_documentation" },
-        ["<Tab>"] = { "fallback" },
-        ["<S-Tab>"] = { "fallback" },
-        ["<C-p>"] = { "snippet_backward", "select_prev", "fallback" },
-        ["<C-n>"] = { "snippet_forward", "select_next", "fallback" },
+return { -- Autocompletion
+  "hrsh7th/nvim-cmp",
+  version = false,
+  event = "InsertEnter",
+  dependencies = {
+    -- Snippet Engine & its associated nvim-cmp source
+    {
+      "L3MON4D3/LuaSnip",
+      build = (function()
+        -- Build Step is needed for regex support in snippets
+        -- This step is not supported in many windows environments
+        -- Remove the below condition to re-enable on windows
+        if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+          return
+        end
+        return "make install_jsregexp"
+      end)(),
+      dependencies = {
+        {
+          "rafamadriz/friendly-snippets",
+          config = function()
+            require("luasnip.loaders.from_vscode").lazy_load()
+          end,
+        },
       },
-
-      highlight = {
-        -- sets the fallback highlight groups to nvim-cmp's highlight groups
-        -- useful for when your theme doesn't support blink.cmp
-        -- will be removed in a future release, assuming themes add support
-        use_nvim_cmp_as_default = true,
+      keys = {
+        {
+          "<tab>",
+          function()
+            return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
+          end,
+          expr = true,
+          silent = true,
+          mode = "i",
+        },
+        {
+          "<tab>",
+          function()
+            require("luasnip").jump(1)
+          end,
+          mode = "s",
+        },
+        {
+          "<s-tab>",
+          function()
+            require("luasnip").jump(-1)
+          end,
+          mode = { "i", "s" },
+        },
       },
-      -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-      -- adjusts spacing to ensure icons are aligned
-      nerd_font_variant = "mono",
-
-      -- experimental auto-brackets support
-      -- accept = { auto_brackets = { enabled = true } }
-
-      -- experimental signature help support
-      -- trigger = { signature_help = { enabled = true } }
     },
-  },
-  -- LSP servers and clients communicate what features they support through "capabilities".
-  --  By default, Neovim support a subset of the LSP specification.
-  --  With blink.cmp, Neovim has *more* capabilities which are communicated to the LSP servers.
-  --  Explanation from TJ: https://youtu.be/m8C0Cq9Uv9o?t=1275
-  --
-  -- This can vary by config, but in-general for nvim-lspconfig:
+    "saadparwaiz1/cmp_luasnip",
 
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = { "saghen/blink.cmp" },
-    config = function(_, opts)
-      local lspconfig = require("lspconfig")
-      for server, config in pairs(opts.servers or {}) do
-        config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
-        lspconfig[server].setup(config)
-      end
-    end,
+    -- Adds other completion capabilities.
+    --  nvim-cmp does not ship with all sources by default. They are split
+    --  into multiple repos for maintenance purposes.
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+
+    -- If you want to add a bunch of pre-configured snippets,
+    --    you can use this plugin to help you. It even has snippets
+    --    for various frameworks/libraries/etc. but you will have to
+    --    set up the ones that are useful for you.
+    -- 'rafamadriz/friendly-snippets',
   },
+  config = function()
+    -- See `:help cmp`
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+    luasnip.setup({
+      history = true,
+      delete_check_events = "TextChanged",
+    })
+    local defaults = require("cmp.config.default")()
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      completion = { completeopt = "menu,menuone,noinsert" },
+
+      -- For an understanding of why these mappings were
+      -- chosen, you will need to read `:help ins-completion`
+      --
+      -- No, but seriously. Please read `:help ins-completion`, it is really good!
+      mapping = cmp.mapping.preset.insert({
+        ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<S-CR>"] = cmp.mapping.confirm({
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = true,
+        }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<C-CR>"] = function(fallback)
+          cmp.abort()
+          fallback()
+        end,
+      }),
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "path" },
+        { name = "luasnip" },
+      }, {
+        { name = "buffer" },
+      }),
+      sorting = defaults.sorting,
+    })
+    cmp.setup.cmdline(":", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = "path" },
+      }, {
+        {
+          name = "cmdline",
+          option = {
+            ignore_cmds = { "Man", "!" },
+          },
+        },
+      }),
+    })
+    -- friendly-snippets - enable standardized comments snippets
+    luasnip.filetype_extend("typescript", { "tsdoc" })
+    luasnip.filetype_extend("javascript", { "jsdoc" })
+  end,
 }
