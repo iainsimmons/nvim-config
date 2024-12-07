80ccebd067acb602504699a62d39606740938597
Iain Simmons <emptiful@gmail.com>
2024-12-07 10:01:31 +1000

    Disable treesitter for large files
    As seen on TJ's Advent of Neovim series on YouTube

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -49,7 +49,17 @@ return {
     },
     opts = {
       auto_install = true,
-      highlight = { enable = true },
+      highlight = {
+        enable = true,
+        -- to disable slow treesitter highlight for large files
+        disable = function(_, buf)
+          local max_filesize = 100 * 1024 -- 100 KB
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            return true
+          end
+        end,
+      },
       indent = { enable = true },
       ensure_installed = {
         "astro",
