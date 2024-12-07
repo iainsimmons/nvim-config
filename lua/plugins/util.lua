f3b96b038c92fdf042e62d15462bff3b9fdcb880
Iain Simmons <emptiful@gmail.com>
2024-12-07 21:21:18 +1000

    Remove vim-startuptime
    lazy.nvim tells me this anyways?

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -1,12 +1,5 @@
 return {
   {
-    "dstein64/vim-startuptime",
-    cmd = "StartupTime",
-    config = function()
-      vim.g.startuptime_tries = 10
-    end,
-  },
-  {
     "folke/persistence.nvim",
     event = "BufReadPre",
     opts = { options = vim.opt.sessionoptions:get() },
