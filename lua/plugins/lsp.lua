b10afd3fa234bbf737bfe1397d437e06ccd84d50
Iain Simmons <emptiful@gmail.com>
2024-12-07 21:19:40 +1000

    Add tiny-inline-diagnostic.nvim

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -214,4 +214,15 @@ return {
       })
     end,
   },
+  {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "VeryLazy",
+    priority = 1000, -- needs to be loaded in first
+    config = function()
+      vim.diagnostic.config({ virtual_text = false })
+      require("tiny-inline-diagnostic").setup({
+        preset = "powerline",
+      })
+    end,
+  },
 }
