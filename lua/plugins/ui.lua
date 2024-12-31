b21277753d4361144acac3e78644c299a7f68490
Iain Simmons <emptiful@gmail.com>
2024-12-31 21:38:37 +1000

    Turn off italics

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -10,6 +10,13 @@ return {
       vim.g.base16_background_transparent = 1
       -- set from https://github.com/FabioAntunes/base16-fish-shell
       vim.cmd("source ~/.vimrc_background")
+      local hl_groups = vim.api.nvim_get_hl(0, {})
+
+      for key, hl_group in pairs(hl_groups) do
+        if hl_group.italic then
+          vim.api.nvim_set_hl(0, key, vim.tbl_extend("force", hl_group, { italic = false }))
+        end
+      end
     end,
   },
   {
