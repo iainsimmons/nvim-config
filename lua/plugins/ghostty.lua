f0a54e85001457ba6acd5c5d6323b800b9782d7e
Iain Simmons <emptiful@gmail.com>
2025-01-03 11:47:10 +1000

    Add ghostty config file syntax highlighting
    From linkarzu https://youtube.com/@linkarzu

---
+++ lua/plugins/ghostty.lua
@@ -0,0 +1,12 @@
+-- Provides syntax highlighting for the ghostty config file
+-- from linkarzu https://youtube.com/@linkarzu
+-- https://github.com/linkarzu/dotfiles-latest/blob/main/neovim/neobean/lua/plugins/ghostty.lua
+-- > No additional dependencies required, Zilchmasta was kind enough to let me
+-- > know about this in discord
+-- > https://discord.com/channels/1005603569187160125/1300462095946485790/1300534513788653630
+
+return {
+  "ghostty",
+  dir = "/Applications/Ghostty.app/Contents/Resources/vim/vimfiles/",
+  lazy = false,
+}
