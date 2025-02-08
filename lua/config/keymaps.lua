fd6650f4dac9aab2e6e9e49d18e6aff2bbb2db4e
Iain Simmons <emptiful@gmail.com>
2025-02-08 11:36:07 +1000

    Remove mini.move
    I find it lags a lot, and I was fine with the basic keymaps I had before

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -26,6 +26,11 @@ map("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 --
 -- [[ Stolen from LazyVim ]]
 --
+--- Move Lines
+map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
+map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move up" })
+map("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
+map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
 
 -- buffers
 map("n", "[b", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
