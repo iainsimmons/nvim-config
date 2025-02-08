089c84a85609a52c8466c288f40b207a6505ff4e
Iain Simmons <emptiful@gmail.com>
2025-02-08 11:23:16 +1000

    Switch to blink.cmp for completions
    …again!
    
    This time, the cmdline completions seem to be working okay!

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,11 +1,18 @@
-return { -- Autocompletion
-  "hrsh7th/nvim-cmp",
-  version = false,
-  event = "InsertEnter",
+-- https://github.com/saghen/blink.cmp
+-- Documentation site: https://cmp.saghen.dev/
+
+-- NOTE: Specify the trigger character(s) used for luasnip
+local trigger_text = ";"
+
+return {
+  "saghen/blink.cmp",
+  version = "*",
+  event = { "BufReadPost", "BufNewFile" },
   dependencies = {
-    -- Snippet Engine & its associated nvim-cmp source
+    "rafamadriz/friendly-snippets",
     {
       "L3MON4D3/LuaSnip",
+      version = "v2.*",
       build = (function()
         -- Build Step is needed for regex support in snippets
         -- This step is not supported in many windows environments
@@ -24,135 +31,226 @@ return { -- Autocompletion
         },
       },
     },
-    "saadparwaiz1/cmp_luasnip",
-
-    -- Adds other completion capabilities.
-    --  nvim-cmp does not ship with all sources by default. They are split
-    --  into multiple repos for maintenance purposes.
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
-
-    -- If you want to add a bunch of pre-configured snippets,
-    --    you can use this plugin to help you. It even has snippets
-    --    for various frameworks/libraries/etc. but you will have to
-    --    set up the ones that are useful for you.
-    -- 'rafamadriz/friendly-snippets',
   },
-  config = function()
-    -- See `:help cmp`
-    local cmp = require("cmp")
-    local luasnip = require("luasnip")
-    luasnip.setup({
-      history = true,
-      delete_check_events = "TextChanged",
-    })
-    local defaults = require("cmp.config.default")()
+  -- In case there are breaking changes and you want to go back to the last
+  -- working release
+  -- https://github.com/Saghen/blink.cmp/releases
+  -- version = "v0.9.3",
+  opts = function(_, opts)
+    opts.enabled = function()
+      -- Get the current buffer's filetype
+      local filetype = vim.bo[0].filetype
+      -- Disable for Telescope buffers
+      if filetype == "TelescopePrompt" or filetype == "minifiles" or filetype == "snacks_picker_input" then
+        return false
+      end
+      return true
+    end
 
-    cmp.setup({
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      },
-      completion = { completeopt = "menu,menuone,noinsert" },
-
-      -- For an understanding of why these mappings were
-      -- chosen, you will need to read `:help ins-completion`
-      --
-      -- No, but seriously. Please read `:help ins-completion`, it is really good!
-      mapping = cmp.mapping.preset.insert({
-        ["<C-n>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.locally_jumpable(1) then
-            luasnip.jump(1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-
-        ["<C-p>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.locally_jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping(function(fallback)
-          -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
-          if cmp.visible() then
-            local entry = cmp.get_selected_entry()
-            if not entry then
-              cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-            end
-            cmp.confirm()
-          else
-            fallback()
-          end
-        end, { "i", "s", "c" }),
-        ["<C-e>"] = cmp.mapping.abort(),
-        ["<CR>"] = cmp.mapping({
-          i = function(fallback)
-            if cmp.visible() and cmp.get_active_entry() then
-              cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
-            else
-              fallback()
-            end
-          end,
-          s = cmp.mapping.confirm({ select = true }),
-          c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
-        }),
-        ["<C-CR>"] = function(fallback)
-          cmp.abort()
-          fallback()
-        end,
-      }),
-      matching = {
-        disallow_fuzzy_matching = false,
-        disallow_fullfuzzy_matching = false,
-        disallow_partial_fuzzy_matching = false,
-        disallow_partial_matching = false,
-      },
-      sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "path" },
-        { name = "luasnip" },
-      }, {
-        { name = "buffer" },
-      }),
-      sorting = defaults.sorting,
-      formatting = {
-        format = require("nvim-highlight-colors").format,
-      },
-    })
-    -- `/` cmdline setup.
-    cmp.setup.cmdline("/", {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = {
-        { name = "buffer" },
-      },
-    })
-    cmp.setup.cmdline(":", {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = cmp.config.sources({
-        { name = "path" },
-      }, {
-        {
-          name = "cmdline",
-          option = {
-            ignore_cmds = { "Man", "!" },
+    -- NOTE: The new way to enable LuaSnip
+    -- Merge custom sources with the existing ones from lazyvim
+    -- NOTE: by default lazyvim already includes the lazydev source, so not adding it here again
+    opts.sources = vim.tbl_deep_extend("force", opts.sources or {}, {
+      default = { "lsp", "path", "snippets", "buffer" },
+      min_keyword_length = function(ctx)
+        -- only applies when typing a command, doesn't apply to arguments
+        if ctx.mode == "cmdline" and string.find(ctx.line, " ") == nil then
+          return 2
+        end
+        return 0
+      end,
+      providers = {
+        lsp = {
+          name = "lsp",
+          enabled = true,
+          module = "blink.cmp.sources.lsp",
+          min_keyword_length = 2,
+          -- When linking markdown notes, I would get snippets and text in the
+          -- suggestions, I want those to show only if there are no LSP
+          -- suggestions
+          --
+          -- Enabled fallbacks as this seems to be working now
+          -- Disabling fallbacks as my snippets wouldn't show up when editing
+          -- lua files
+          -- fallbacks = { "snippets", "buffer" },
+          score_offset = 90, -- the higher the number, the higher the priority
+        },
+        path = {
+          name = "Path",
+          module = "blink.cmp.sources.path",
+          score_offset = 25,
+          -- When typing a path, I would get snippets and text in the
+          -- suggestions, I want those to show only if there are no path
+          -- suggestions
+          fallbacks = { "snippets", "buffer" },
+          min_keyword_length = 2,
+          opts = {
+            trailing_slash = false,
+            label_trailing_slash = true,
+            get_cwd = function(context)
+              return vim.fn.expand(("#%d:p:h"):format(context.bufnr))
+            end,
+            show_hidden_files_by_default = true,
           },
         },
-      }),
+        buffer = {
+          name = "Buffer",
+          enabled = true,
+          max_items = 3,
+          module = "blink.cmp.sources.buffer",
+          min_keyword_length = 4,
+          score_offset = 15, -- the higher the number, the higher the priority
+        },
+        snippets = {
+          name = "snippets",
+          enabled = true,
+          max_items = 15,
+          min_keyword_length = 2,
+          module = "blink.cmp.sources.snippets",
+          score_offset = 85, -- the higher the number, the higher the priority
+          -- Only show snippets if I type the trigger_text characters, so
+          -- to expand the "bash" snippet, if the trigger_text is ";" I have to
+          should_show_items = function(ctx)
+            return ctx.trigger.initial_kind ~= "trigger_character"
+          end,
+        },
+      },
+      cmdline = function()
+        local type = vim.fn.getcmdtype()
+        if type == "/" or type == "?" then
+          return { "buffer" }
+        end
+        if type == ":" then
+          return { "cmdline" }
+        end
+        return {}
+      end,
     })
-    -- friendly-snippets - enable standardized comments snippets
-    luasnip.filetype_extend("typescript", { "tsdoc" })
-    luasnip.filetype_extend("javascript", { "jsdoc" })
+
+    opts.completion = {
+      --   keyword = {
+      --     -- 'prefix' will fuzzy match on the text before the cursor
+      --     -- 'full' will fuzzy match on the text before *and* after the cursor
+      --     -- example: 'foo_|_bar' will match 'foo_' for 'prefix' and 'foo__bar' for 'full'
+      --     range = "full",
+      --   },
+      menu = {
+        border = "single",
+        draw = {
+          columns = { { "item_idx" }, { "kind_icon" }, { "label", "label_description", gap = 1 } },
+          components = {
+            item_idx = {
+              text = function(ctx)
+                return ctx.idx == 10 and "0" or ctx.idx >= 10 and " " or tostring(ctx.idx)
+              end,
+              highlight = "BlinkCmpItemIdx", -- optional, only if you want to change its color
+            },
+            kind_icon = {
+              ellipsis = false,
+              text = function(ctx)
+                local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
+                return kind_icon
+              end,
+              -- Optionally, you may also use the highlights from mini.icons
+              highlight = function(ctx)
+                local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+                return hl
+              end,
+            },
+          },
+        },
+      },
+      documentation = {
+        auto_show = false,
+        window = {
+          border = "single",
+        },
+      },
+    }
+    opts.signature = { window = { border = "single" } }
+
+    -- opts.fuzzy = {
+    --   -- Disabling this matches the behavior of fzf
+    --   use_typo_resistance = false,
+    --   -- Frecency tracks the most recently/frequently used items and boosts the score of the item
+    --   use_frecency = true,
+    --   -- Proximity bonus boosts the score of items matching nearby words
+    --   use_proximity = false,
+    -- }
+
+    opts.snippets = {
+      preset = "luasnip",
+    }
+
+    -- The default preset used by lazyvim accepts completions with enter
+    -- I don't like using enter because if on markdown and typing
+    -- something, but you want to go to the line below, if you press enter,
+    -- the completion will be accepted
+    -- https://cmp.saghen.dev/configuration/keymap.html#default
+    opts.keymap = {
+      preset = "default",
+      ["<C-space>"] = { "show", "accept", "show_documentation", "hide_documentation" },
+      ["<Tab>"] = { "snippet_forward", "fallback" },
+      ["<S-Tab>"] = { "snippet_backward", "fallback" },
+      ["<C-p>"] = { "snippet_backward", "select_prev", "fallback" },
+      ["<C-n>"] = { "snippet_forward", "select_next", "fallback" },
+      ["<Up>"] = { "select_prev", "fallback" },
+      ["<Down>"] = { "select_next", "fallback" },
+      ["<S-k>"] = { "scroll_documentation_up", "fallback" },
+      ["<S-j>"] = { "scroll_documentation_down", "fallback" },
+      ["<C-e>"] = { "hide", "fallback" },
+      ["<A-1>"] = {
+        function(cmp)
+          cmp.accept({ index = 1 })
+        end,
+      },
+      ["<A-2>"] = {
+        function(cmp)
+          cmp.accept({ index = 2 })
+        end,
+      },
+      ["<A-3>"] = {
+        function(cmp)
+          cmp.accept({ index = 3 })
+        end,
+      },
+      ["<A-4>"] = {
+        function(cmp)
+          cmp.accept({ index = 4 })
+        end,
+      },
+      ["<A-5>"] = {
+        function(cmp)
+          cmp.accept({ index = 5 })
+        end,
+      },
+      ["<A-6>"] = {
+        function(cmp)
+          cmp.accept({ index = 6 })
+        end,
+      },
+      ["<A-7>"] = {
+        function(cmp)
+          cmp.accept({ index = 7 })
+        end,
+      },
+      ["<A-8>"] = {
+        function(cmp)
+          cmp.accept({ index = 8 })
+        end,
+      },
+      ["<A-9>"] = {
+        function(cmp)
+          cmp.accept({ index = 9 })
+        end,
+      },
+      ["<A-0>"] = {
+        function(cmp)
+          cmp.accept({ index = 10 })
+        end,
+      },
+    }
+
+    return opts
   end,
 }
