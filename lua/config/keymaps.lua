1aa5645ec320be9f9486af5ae35e8d4e96ea46ba
Iain Simmons <emptiful@gmail.com>
2025-02-12 16:33:22 +1000

    Remove next/prev search result keymaps
    These must be clashing with something else because it would often just
    recentre on the current result instead of actually going to the next
    result

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -40,14 +40,6 @@ map("n", "<S-Tab>", "<cmd>bprev<CR>", { desc = "Previous Buffer", silent = true,
 map("n", "<leader>bb", "<cmd>b#<cr>", { desc = "Switch to Other Buffer" })
 map("n", "<leader><Tab>", "<cmd>b#<cr>", { desc = "Switch to Other Buffer" })
 
--- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
-map("n", "n", "'Nn'[v:searchforward].'zv'", { expr = true, desc = "Next search result" })
-map("x", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
-map("o", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
-map("n", "N", "'nN'[v:searchforward].'zv'", { expr = true, desc = "Prev search result" })
-map("x", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
-map("o", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
-
 -- Add undo break-points
 map("i", ",", ",<c-g>u")
 map("i", ".", ".<c-g>u")
