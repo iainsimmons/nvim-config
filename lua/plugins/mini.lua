c58a2c1994c76f92c8a2c69e5cb2d96b0ece7300
Iain Simmons <emptiful@gmail.com>
2025-03-20 16:42:09 +1000

    Simplify mini.statusline config
    I prefer the short form of most sections, so setting trunc_width to 200
    seems to do the trick

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -153,22 +153,18 @@ return { -- Collection of various small independent plugins/modules
             end
           end
 
-          local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
-          local git = MiniStatusline.section_git({ trunc_width = 40 })
-          local diff = MiniStatusline.section_diff({ trunc_width = 75 })
+          local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 200 })
+          local diff = MiniStatusline.section_diff({ trunc_width = 75, icon = "󰊢 " })
           local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
-          -- local lsp = MiniStatusline.section_lsp({ trunc_width = 75 })
-          local filename = MiniStatusline.section_filename({ trunc_width = 140 })
-          local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+          local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 200 })
           local location = MiniStatusline.section_location({ trunc_width = 200 })
           local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
           local macro = check_macro_recording()
 
           return MiniStatusline.combine_groups({
             { hl = mode_hl, strings = { mode } },
-            { hl = "MiniStatuslineDevinfo", strings = { git, diff, diagnostics } },
+            { hl = "MiniStatuslineDevinfo", strings = { diff, diagnostics } },
             "%<", -- Mark general truncate point
-            { hl = "MiniStatuslineFilename", strings = { filename } },
             "%=", -- End left alignment
             { hl = "MiniStatuslineFilename", strings = { macro } },
             { hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
