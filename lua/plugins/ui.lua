f1b8225913976ced469867438586b7661dbeadef
Iain Simmons <emptiful@gmail.com>
2025-03-21 21:38:27 +1000

    Use Noice's mini view
    From linkarzu's video of Neovim plugins (2025):
    https://youtu.be/08AYe8cMffI
    
    And his dotfiles:
    https://github.com/linkarzu/dotfiles-latest/blob/main/neovim/neobean/lua/plugins/noice.lua

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -22,6 +22,30 @@ return {
           ["vim.lsp.util.stylize_markdown"] = true,
           ["cmp.entry.get_documentation"] = true,
         },
+        message = {
+          -- Messages shown by lsp servers
+          enabled = true,
+          view = "mini",
+        },
+      },
+      messages = {
+        -- NOTE: If you enable messages, then the cmdline is enabled automatically.
+        -- This is a current Neovim limitation.
+        enabled = true, -- enables the Noice messages UI
+        view = "mini", -- default view for messages
+        view_error = "mini", -- view for errors
+        view_warn = "mini", -- view for warnings
+        view_history = "mini", -- view for :messages
+        view_search = "mini", -- view for search count messages. Set to `false` to disable
+      },
+      notify = {
+        -- Noice can be used as `vim.notify` so you can route any notification like other messages
+        -- Notification messages have their level and other properties set.
+        -- event is always "notify" and kind can be any log level as a string
+        -- The default routes will forward notifications to nvim-notify
+        -- Benefit of using Noice for this is the routing and consistent history view
+        enabled = true,
+        view = "mini",
       },
       routes = {
         {
@@ -49,6 +73,17 @@ return {
         long_message_to_split = true, -- long messages will be sent to a split
         inc_rename = true, -- enables an input dialog for inc-rename.nvim
         lsp_doc_border = true, -- add a border to hover docs and signature help
+      },
+      views = {
+        mini = {
+          align = "center",
+          position = {
+            -- Centers messages top to bottom
+            row = -2,
+            -- Aligns messages to the far right
+            col = "100%",
+          },
+        },
       },
     },
     keys = {
