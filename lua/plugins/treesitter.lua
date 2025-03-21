e817a520c569badbea32f14ee2778c495be3df4c
Iain Simmons <emptiful@gmail.com>
2025-03-21 21:17:33 +1000

    Add treesitter context toggle
    From LazyVim's method for this:
    https://www.lazyvim.org/extras/ui/treesitter-context#nvim-treesitter-context

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -15,7 +15,24 @@ return {
       require("nvim-treesitter.query_predicates")
     end,
     dependencies = {
-      { "nvim-treesitter/nvim-treesitter-context", opts = { mode = "cursor", max_lines = 3 } },
+      {
+        "nvim-treesitter/nvim-treesitter-context",
+        opts = function()
+          local tsc = require("treesitter-context")
+          Snacks.toggle({
+            name = "Treesitter Context",
+            get = tsc.enabled,
+            set = function(state)
+              if state then
+                tsc.enable()
+              else
+                tsc.disable()
+              end
+            end,
+          }):map("<leader>ut")
+          return { mode = "cursor", max_lines = 3 }
+        end,
+      },
       {
         "nvim-treesitter/nvim-treesitter-textobjects",
         config = function()
