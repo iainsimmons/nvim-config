c734d345fc0723941e34454c798e426ccec052d6
Iain Simmons <emptiful@gmail.com>
2025-04-06 13:24:46 +1000

    Add nvim-ufo for folds
    Stolen config from Nikolov Lazar
    https://github.com/nikolovlazar/dotfiles/blob/main/.config/nvim/lua/plugins/editor/ufo.lua
    and seen in the YouTube video with linkarzu and Nikolov:
    https://youtu.be/TFvB74fd0as

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -100,4 +100,44 @@ return {
       },
     },
   },
+  {
+    "kevinhwang91/nvim-ufo",
+    event = "BufRead",
+    keys = {
+      {
+        "zR",
+        function()
+          require("ufo").openAllFolds()
+        end,
+      },
+      {
+        "zM",
+        function()
+          require("ufo").closeAllFolds()
+        end,
+      },
+      {
+        "K",
+        function()
+          local winid = require("ufo").peekFoldedLinesUnderCursor()
+          if not winid then
+            vim.lsp.buf.hover()
+          end
+        end,
+      },
+    },
+    dependencies = {
+      { "kevinhwang91/promise-async" },
+    },
+    config = function()
+      -- Fold options
+      vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+      vim.o.foldcolumn = "1" -- '0' is not bad
+      vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+
+      require("ufo").setup()
+    end,
+  },
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -42,7 +42,7 @@ return {
 
           -- Opens a popup that displays documentation about the word under your cursor
           --  See `:help K` for why this keymap
-          map("K", vim.lsp.buf.hover, "Hover Documentation")
+          -- map("K", vim.lsp.buf.hover, "Hover Documentation")
 
           -- WARN: This is not Goto Definition, this is Goto Declaration.
           --  For example, in C this would take you to the header
