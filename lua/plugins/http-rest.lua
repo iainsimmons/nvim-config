531bc85252d4c403660d4c2a3fa5ff06ede1114f
Iain Simmons <emptiful@gmail.com>
2025-05-15 22:55:49 +1000

    Add comments to kulala.nvim options
    And turn off summary

--- lua/plugins/http-rest.lua
+++ lua/plugins/http-rest.lua
@@ -1,6 +1,7 @@
 return {
   {
     "mistweaverco/kulala.nvim",
+    branch = "develop",
     keys = {
       {
         "<CR>",
@@ -15,10 +16,25 @@ return {
     opts = {
       global_keymaps = true,
       ui = {
-        default_view = "headers_body",
-        -- display_mode = "float",
+        -- default view: "body" or "headers" or "headers_body" or "verbose" or fun(response: Response)
+        default_view = "body",
+
+        -- display mode: possible values: "split", "float"
+        display_mode = "split",
+
+        -- set scope for environment and request variables
+        -- possible values: b = buffer, g = global
         environment_scope = "g",
-        -- show_variable_info_text = "float",
+
+        -- enable/disable request summary in the output window
+        show_request_summary = false,
+
+        -- enable/disable variable info text
+        -- this will show the variable name and value as float
+        -- possible values: false, "float"
+        show_variable_info_text = false,
+
+        -- split direction: possible values: "vertical", "horizontal"
         split_direction = "horizontal",
       },
     },
