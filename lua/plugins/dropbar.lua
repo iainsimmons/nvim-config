5f2c2a7e22176af5db366d441f6df2a36055d99c
Iain Simmons <emptiful@gmail.com>
2025-12-02 11:29:01 +1000

    Show filename in dropbar for more file types

--- lua/plugins/dropbar.lua
+++ lua/plugins/dropbar.lua
@@ -213,6 +213,37 @@ return {
         },
       },
       bar = {
+        enable = function(buf, win, _)
+          buf = vim._resolve_bufnr(buf)
+
+          if not vim.api.nvim_buf_is_valid(buf) or not vim.api.nvim_win_is_valid(win) then
+            return false
+          end
+
+          if
+            not vim.api.nvim_buf_is_valid(buf)
+            or not vim.api.nvim_win_is_valid(win)
+            or vim.fn.win_gettype(win) ~= ""
+            or vim.wo[win].winbar ~= ""
+            or vim.bo[buf].ft == "help"
+          then
+            return false
+          end
+
+          local stat = vim.uv.fs_stat(vim.api.nvim_buf_get_name(buf))
+          if stat and stat.size > 1024 * 1024 then
+            return false
+          end
+
+          return vim.bo[buf].ft == "markdown"
+            or vim.bo[buf].ft == "text"
+            or vim.bo[buf].ft == "conf"
+            or pcall(vim.treesitter.get_parser, buf)
+            or not vim.tbl_isempty(vim.lsp.get_clients({
+              bufnr = buf,
+              method = "textDocument/documentSymbol",
+            }))
+        end,
         sources = function()
           return { smart_path, mini_diff_stats, lsp_diagnostics }
         end,
