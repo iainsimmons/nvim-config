ef3720b78e798b34866229985be5c7174330f951
Iain Simmons <emptiful@gmail.com>
2026-01-01 22:40:59 +1000

    Try new colorscheme mistweaverco/vhs-era-theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,30 @@
-return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {
-    style = "night",
-    transparent = true,
-  },
-  config = function(_, opts)
-    require("tokyonight").setup(opts)
-    vim.cmd([[colorscheme tokyonight]])
-  end,
-}
+if false then
+  return {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = "night",
+      transparent = true,
+    },
+    config = function(_, opts)
+      require("tokyonight").setup(opts)
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  }
+else
+  return {
+    "mistweaverco/vhs-era-theme.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      italic_comments = false,
+      disable_cache = false,
+      hot_reload = false,
+    },
+    config = function(_, opts)
+      require("vhs-era-theme").setup(opts)
+      vim.cmd([[colorscheme vhs-era-theme]])
+    end,
+  }
+end
