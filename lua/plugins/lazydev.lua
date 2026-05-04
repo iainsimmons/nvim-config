eb16a230ca4be2d4127aaf6831c808e547149a0d
Iain Simmons <emptiful@gmail.com>
2026-05-04 22:19:03 +1000

    Remove wezterm-types and smear-cursor
    No longer needed after switching to kitty

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -2,19 +2,12 @@ return {
   {
     "folke/lazydev.nvim",
     ft = "lua", -- only load on lua files
-    dependencies = {
-      { "gonstoll/wezterm-types", lazy = true },
-    },
     opts = {
       library = {
         -- See the configuration section for more details
         -- Load luvit types when the `vim.uv` word is found
         { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-        -- Load the wezterm types when the `wezterm` module is required
-        -- Needs `justinsgithub/wezterm-types` to be installed
-        { path = "wezterm-types", mods = { "wezterm" } },
       },
     },
   },
-  { "justinsgithub/wezterm-types", lazy = true }, -- optional `vim.uv` typings
 }
