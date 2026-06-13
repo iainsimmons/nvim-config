b5fec4557c65f766bd060f67a31ffe618324afd4
Iain Simmons <emptiful@gmail.com>
2026-06-13 10:45:46 +1000

    Add diffconflicts.nvim

--- lua/plugins/diff.lua
+++ lua/plugins/diff.lua
@@ -1,8 +1,38 @@
 return {
-  "esmuellert/vscode-diff.nvim",
-  cmd = { "CodeDiff" },
-  dependencies = { "MunifTanjim/nui.nvim" },
-  keys = {
-    { "<leader>gd", "<cmd>CodeDiff<CR>", desc = "CodeDiff", silent = true },
+  {
+    "esmuellert/vscode-diff.nvim",
+    cmd = { "CodeDiff" },
+    dependencies = { "MunifTanjim/nui.nvim" },
+    keys = {
+      { "<leader>gd", "<cmd>CodeDiff<CR>", desc = "CodeDiff", silent = true },
+    },
+  },
+  {
+    "mistweaverco/diffconflicts.nvim",
+    event = "VeryLazy",
+    cmd = { "DiffConflicts", "DiffConflictsShowHistory", "DiffConflictsWithHistory" },
+    opts = {
+      -- Optional configuration
+      commands = {
+        -- Command to open the diff conflicts view, default is "DiffConflicts"
+        -- set to nil to disable the command
+        diff_conflicts = "DiffConflicts",
+        -- Command to show the history of conflicts, default is "DiffConflictsShowHistory"
+        -- set to nil to disable the command
+        show_history = "DiffConflictsShowHistory",
+        -- Command to resolve conflicts with history, default is "DiffConflictsWithHistory"
+        -- set to nil to disable the command
+        with_history = "DiffConflictsWithHistory",
+      },
+      -- Quality-of-life options
+      qol = {
+        -- After saving (:w), automatically close the diff view and jump to the next
+        -- conflict in the file (if any).
+        advance_on_save = true,
+        -- If no conflicts remain after saving, quit Neovim (:qa). This is useful
+        -- when running from `git mergetool` / `jj resolve`.
+        quit_on_done = true,
+      },
+    },
   },
 }
